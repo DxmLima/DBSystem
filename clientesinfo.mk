7bce3b5961cfa11ff41c49e6e1def77bfcf76106
Mateus Lima <65178646+DxmLima@users.noreply.github.com>
2023-04-16 16:44:59 -0300

    Update clientesinfo.mk

--- clientesinfo.mk
+++ clientesinfo.mk
@@ -3,3 +3,5 @@
 |  0 | Ricardo Rassmusen        | 01 04 2000        | Avenida Brasil 432  | 50419942-02 | 59919452943-24 |          1 |
 |  1 | Eunaldo gustov rodrigues | 01 04 2003        | Rua Afonso Bandeira | 555555555   | 125655555-55   |          2 |
 |  2 | Euvira Franscion         | 01 06 1994        | Soroacabanos 1442   | 4223444102  | 1852924959044  |          3 |
+
+// TODOS OS DADOS SÃO EXEMPLOS, QUALQUER SEMELHANÇA OU IGUALIDADE É MERA COINCIDÊNCIA 
